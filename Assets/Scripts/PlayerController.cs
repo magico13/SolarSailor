23b57adb91d63c21da84cbac1194dd1b57d498bb
magico13 <magico1313@gmail.com>
2020-10-02 23:42:47 -0400

    Actual real first commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.UIElements;
+
+public class PlayerController : MonoBehaviour
+{
+    public float ThrowForce;
+    public float Rotation;
+    public GameObject Wrench;
+    public AudioClip ThrowSound;
+
+    private AudioSource _audio;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Physics2D.gravity = new Vector2(0, -2f);
+        _audio = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if the player clicks, figure out the direction, apply force in the opposite direction
+
+        if (Input.GetMouseButtonDown((int)MouseButton.LeftMouse))
+        {
+            Vector3 worldMousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            Vector2 direction = worldMousePos - transform.position;
+            direction.Normalize();
+
+            gameObject.GetComponent<Rigidbody2D>().AddForce(-ThrowForce*direction);
+
+            //create wrench object
+            GameObject created = Instantiate(Wrench, transform.position + new Vector3(Mathf.Sign(direction.x)*0.5f, 0), Quaternion.identity);
+            created.SetActive(true);
+            Rigidbody2D rigid = created.GetComponent<Rigidbody2D>();
+            rigid.velocity = GetComponent<Rigidbody2D>().velocity;
+            rigid.AddForce(ThrowForce * direction);
+            rigid.AddTorque(Random.Range(-Rotation, Rotation));
+
+            _audio.PlayOneShot(ThrowSound);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/WrenchController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class WrenchController : MonoBehaviour
+{
+    public float DestroyDelay = 0.5f;
+    public AudioClip HitWallSound;
+
+    private AudioSource _audio;
+    void Start()
+    {
+        _audio = GetComponent<AudioSource>();
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider?.gameObject?.CompareTag("Player") != true)
+        {
+            _audio.PlayOneShot(HitWallSound, collision.relativeVelocity.magnitude/10f);
+            Destroy(gameObject, DestroyDelay);
+        }
+    }
+}
