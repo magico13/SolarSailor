10b5d5cacaa879bd5b8fedb0972a2a327eb51b7a
magico13 <magico1313@gmail.com>
2020-10-03 12:59:35 -0400

    Start adding textures, turrets, and level.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,19 +3,22 @@ using UnityEngine.UIElements;
 
 public class PlayerController : MonoBehaviour
 {
+    public float MaxSpeed;
     public float ThrowForce;
     public float Rotation;
     public GameObject Wrench;
     public AudioClip ThrowSound;
 
     private AudioSource _audio;
-
-
+    private float _distToGround;
+    private Rigidbody2D _body;
     // Start is called before the first frame update
     void Start()
     {
-        Physics2D.gravity = new Vector2(0, -2f);
+        //Physics2D.gravity = new Vector2(0, -2f);
         _audio = GetComponent<AudioSource>();
+        _distToGround = GetComponent<Collider2D>().bounds.extents.y + 0.1f;
+        _body = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -30,7 +33,7 @@ public class PlayerController : MonoBehaviour
             Vector2 direction = worldMousePos - transform.position;
             direction.Normalize();
 
-            gameObject.GetComponent<Rigidbody2D>().AddForce(-ThrowForce*direction);
+            _body.AddForce(-ThrowForce*direction);
 
             //create wrench object
             GameObject created = Instantiate(Wrench, transform.position + new Vector3(Mathf.Sign(direction.x)*0.5f, 0), Quaternion.identity);
@@ -39,13 +42,32 @@ public class PlayerController : MonoBehaviour
             rigid.velocity = GetComponent<Rigidbody2D>().velocity;
             rigid.AddForce(ThrowForce * direction);
             rigid.AddTorque(Random.Range(-Rotation, Rotation));
+            Destroy(created, 10f);
 
             _audio.PlayOneShot(ThrowSound);
         }
+        if (Input.GetButtonDown("Jump") && IsGrounded())
+        {
+            _body.AddForce(ThrowForce * 3 * Vector2.up);
+        }
+    }
+
+    bool IsGrounded()
+    {
+        RaycastHit2D raycast = Physics2D.Raycast(transform.position, Vector2.down, _distToGround, LayerMask.GetMask("Default"));
+        //Debug.DrawLine(transform.position, raycast.point);
+        return raycast.collider != null && raycast.distance < _distToGround;
     }
 
     void FixedUpdate()
     {
-        
+        if (Input.GetButton("Horizontal") && IsGrounded())
+        {
+            float horizontal = Input.GetAxis("Horizontal");
+            if (Mathf.Abs(_body.velocity.x) < MaxSpeed)
+            {
+                _body.AddForce(ThrowForce * horizontal * Vector2.right);
+            }
+        }
     }
 }
---
+++ Assets/Scripts/TurretController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretController : MonoBehaviour
+{
+
+    GameObject ThePlayer;
+    GameObject Wrench;
+
+    float shootTimeout = 1f;
+    float shootCounter = 0f;
+    bool canShoot = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ThePlayer = FindObjectOfType<PlayerController>().gameObject;
+        Wrench = FindObjectOfType<PlayerController>().Wrench;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // look for player, try to shoot them
+        //Debug.DrawLine(transform.position, ThePlayer.transform.position);
+        if (!canShoot)
+        {
+            shootCounter += Time.deltaTime;
+            canShoot = shootCounter > shootTimeout;
+            return;
+        }
+        var raycast = Physics2D.Raycast(transform.position, ThePlayer.transform.position - transform.position);
+        Debug.DrawLine(transform.position, raycast.point);
+        if (raycast.collider?.gameObject?.CompareTag("Player") == true)
+        {
+            shootCounter = 0;
+
+            GameObject player = raycast.collider.gameObject;
+            //is player, try to shoot
+            Vector2 direction = (transform.position - player.transform.position).normalized;
+            GameObject created = Instantiate(Wrench, transform.position + new Vector3(Mathf.Sign(direction.x) * 0.5f, 0), Quaternion.identity);
+            created.SetActive(true);
+            Rigidbody2D rigid = created.GetComponent<Rigidbody2D>();
+            rigid.velocity = GetComponent<Rigidbody2D>().velocity;
+            rigid.AddForce(150 * direction);
+            Destroy(created, 10f);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider?.gameObject?.CompareTag("Weapon") == true)
+        {
+            //_audio.PlayOneShot(HitWallSound, collision.relativeVelocity.magnitude / 10f);
+            Destroy(gameObject);
+            Destroy(collision.collider.gameObject);
+        }
+    }
+}
